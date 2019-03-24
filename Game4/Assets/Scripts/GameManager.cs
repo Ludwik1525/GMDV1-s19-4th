48d7227addb96350d808c3aa19e06fa64a56a340
Ludwik1526 <mmichal1526@gmail.com>
2019-03-24 19:08:40 +0100

    Project creation, basic scenes

---
+++ Game4/Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Debug = UnityEngine.Debug;
+
+[System.Serializable]
+public class GameManager : MonoBehaviour
+{
+
+    private string configFilePath = "Assets/Resources/config.JSON";
+
+	void Start () {
+		LoadConfigFile();
+	}
+
+	void Update () {
+	}
+
+   
+    public void LoadConfigFile()
+    {
+        if (File.Exists(configFilePath))
+        {
+            string dataInJson = File.ReadAllText(configFilePath);
+            string loadedData = JsonUtility.FromJson<string>(dataInJson);
+
+            Debug.Log(dataInJson);
+        }
+        else
+        {
+            Debug.LogError("Config file broken!");
+        }
+    }
+
+    void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    void LoadMainScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+}
---
+++ Game4/Assets/Scripts/StartSceneButtons.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartSceneButtons : MonoBehaviour
+{
+
+    public Button newGameButton;
+    public Button highscoreButton;
+    public Button controlsButton;
+    public Button exitButton;
+    public Button backButton;
+
+    public GameObject menuButtons;
+    public GameObject highscoreMenu;
+    public GameObject controlsMenu;
+
+	void Start () {
+
+        menuButtons.gameObject.SetActive(true);
+        highscoreMenu.gameObject.SetActive(false);
+        controlsMenu.gameObject.SetActive(false);
+        backButton.gameObject.SetActive(false);
+
+        newGameButton.onClick.AddListener(StartNewGame);
+        highscoreButton.onClick.AddListener(OpenHighScore);
+        controlsButton.onClick.AddListener(OpenControls);
+        exitButton.onClick.AddListener(ExitGame);
+        backButton.onClick.AddListener(BackToMenu);
+	}
+	
+	void Update () {
+		
+	}
+
+    void StartNewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void OpenHighScore()
+    {
+        menuButtons.gameObject.SetActive(false);
+        highscoreMenu.gameObject.SetActive(true);
+        controlsMenu.gameObject.SetActive(false);
+        backButton.gameObject.SetActive(true);
+    }
+    
+    void OpenControls()
+    {
+        menuButtons.gameObject.SetActive(false);
+        highscoreMenu.gameObject.SetActive(false);
+        controlsMenu.gameObject.SetActive(true);
+        backButton.gameObject.SetActive(true);
+    }
+
+    void BackToMenu()
+    {
+        menuButtons.gameObject.SetActive(true);
+        highscoreMenu.gameObject.SetActive(false);
+        controlsMenu.gameObject.SetActive(false);
+        backButton.gameObject.SetActive(false);
+    }
+
+    void ExitGame()
+    {
+        Application.Quit();
+    }
+}
