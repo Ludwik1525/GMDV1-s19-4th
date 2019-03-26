630833dd9bef49b9dd42f2ad9a8a42c08d0e1186
Ludwik1526 <mmichal1526@gmail.com>
2019-03-26 13:48:08 +0100

    Worm + its movement

--- Game4/Assets/Scripts/PlayerAnimation.cs
+++ Game4/Assets/Scripts/PlayerAnimation.cs
@@ -19,22 +19,22 @@ public class PlayerAnimation : MonoBehaviour
 	
 	void Update () {
 
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             PlaySound(walkingSound);
             anim.Play("CatGoUp");
         }
-        else if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             PlaySound(walkingSound);
             anim.Play("CatGoDown");
         }
-        else if (Input.GetKey(KeyCode.A))
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             PlaySound(walkingSound);
             anim.Play("CatGoLeft");
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             PlaySound(walkingSound);
             anim.Play("CatGoRight");
---
+++ Game4/Assets/Scripts/WormMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormMovement : MonoBehaviour {
+
+    [Range(1f, 4f)]
+    public float speed;
+
+    [Range(1f, 5f)]
+    public float changeSideTime;
+
+    private Vector2 velocity;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    private bool facingRight = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        moveInput = new Vector2(speed, 0);
+        StartCoroutine(WormCoroutine(changeSideTime));
+    }
+
+    void Update()
+    {
+        if (!facingRight)
+        {
+            moveInput = new Vector2(-speed, 0);
+        }
+
+        if (facingRight)
+        {
+            moveInput = new Vector2(speed, 0);
+        }
+        
+        velocity = moveInput.normalized * speed * 50;
+
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+    }
+
+    void Flip()
+    {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+        facingRight = !facingRight;
+    }
+
+    public IEnumerator WormCoroutine(float t)
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(t);
+            Flip();
+        }
+    }
+}
